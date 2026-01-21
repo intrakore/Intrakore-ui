1415d28060735894f49c4405b3964b9a0b5f510a
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2026-01-21 18:02:37 +0530

    fix: revert unnecessary changes

--- components/TagInput/types.ts
+++
@@ -1,8 +0,0 @@
-import { SimpleOption } from "../Combobox/types"
-
-export interface TagInputProps {
-  modelValue?: string | null
-  placeholder?: string
-  disabled?: boolean
-  renderIcon?: Function
-}
